6084b1a388d33e08cc198b22162e4b6bddcdb524
Bob Briody <rjbriody@mgmail.com>
2014-09-17 09:10:24 -0400

    minor doc update

--- src/main/java/com/thinkaurelius/titan/webexample/RequestWrapper.java
+++ src/main/java/com/thinkaurelius/titan/webexample/RequestWrapper.java
@@ -12,6 +12,10 @@ import org.springframework.stereotype.Component;
  * This class intercepts all incoming requests and outgoing responses. Essentially, it wraps all Servlet traffic. A
  * rollback is executed at the beginning and end of each Request.
  * <p/>
+ * This strategy relies on 2 things: 1) Titan will create a thread-local transaction automatically when the first
+ * operation is performed 2) Each request must be handled synchronously by a single thread. The later may vary from
+ * container to container. Check the specification for your container.
+ * <p/>
  * In practice, you will need to choose a transaction scheme suitable for your use cases. Performing a rollback before
  * and after each request is only suitable for a read-only application. If your application performs writes then they
  * will need to be explicitly committed within your application code.
